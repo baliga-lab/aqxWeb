b18d446878124c710fa76d022f51879f2f73ba39
Wei-ju Wu <weiju.wu@gmail.com>
2015-08-20 09:13:23 -0700

    outfactored REST API to separate Flask Blueprint

--- app.py
+++ app.py
@@ -16,14 +16,12 @@ import requests
 
 import aqxdb
 import csvimport
-
-"""This is the prototype web application for our Aquaponics site.
-We might consider later splitting stuff up into Flask Blueprints
-"""
+from aqx_api import aqx_api
 
 
 app = Flask(__name__)
 app.config.from_envvar('AQUAPONICS_SETTINGS')
+app.register_blueprint(aqx_api)
 
 
 ######################################################################
@@ -36,11 +34,6 @@ def dbconn():
                            passwd=app.config['PASS'], db=app.config['DB'])
 
 
-def google_user_info(bearer_token):
-    resp = requests.get(app.config['USERINFO_URL'], headers={'Authorization': 'Bearer %s' % bearer_token})
-    return resp.json()
-
-
 def get_user(google_id, email):
     conn = dbconn()
     cursor = conn.cursor()
@@ -48,33 +41,6 @@ def get_user(google_id, email):
         return aqxdb.get_or_create_user(conn, cursor, google_id, email)
     finally:
         conn.close()
-
-
-def authorize(func):
-    """authorization wrapper"""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        auth = request.headers.get('Authorization')
-        if auth is not None:
-            auth = auth.split()
-            app.logger.debug("@authorize auth: %s", str(auth))
-            if auth[0] == 'Bearer':
-                user_info = google_user_info(auth[1])            
-                app.logger.debug(user_info)
-                if 'error' in user_info:
-                    app.logger.error('error in authorization: %s', user_info['error']['message'])
-                else:
-                    kwargs['google_id'] = user_info['id']
-                    app.logger.debug('@authorize(), google_id: %s', kwargs['google_id'])
-            else:
-                # error wrong authentification
-                app.logger.error('@authorize(), not a Bearer token')
-                return jsonify(error="authorization error")
-        else:
-            app.logger.debug("no authorization headers: %s", str(request.headers))
-            return jsonify(error="authorization error")
-        return func(*args, **kwargs)
-    return wrapper
 
 
 def requires_login(func):
@@ -447,66 +413,6 @@ def import_csv():
     return redirect(url_for('sys_details', system_uid=sys_uid))
 
 
-######################################################################
-#### REST API
-#### TODO: maybe put into its own Blueprint
-######################################################################
-
-API_TIME_FORMAT = '%m/%d/%Y %H:%M:%S'
-
-"""
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ya29.0QG7E0h26h9tXDBb5pruN7bJJjtxDeFAC_u5oFTqCYf4pZDfSHoV21DRJi31152dNJwyuA" -d '[{"time": "08/15/2015 08:15:30", "o2": 10.2}]' http://localhost:5000/api/v1/add-measurements/7921a6763e0011e5beb064273763ec8b
-
-"""
-@app.route('/api/v1/add-measurements/<system_id>', methods=['POST'])
-@authorize
-def api_add_measurements(system_id, *args, **kwargs):
-    conn = dbconn()
-    cursor = conn.cursor()
-    try:
-        if aqxdb.is_system_owner(cursor, system_id, google_id=kwargs['google_id']):
-            app.logger.debug('adding measurements for system id: %s and google id: %s', system_id, kwargs['google_id']) 
-            measurements = json.loads(request.data)
-            app.logger.debug(measurements)
-            try:
-                warned = False
-                for measurement in measurements:
-                    if not 'time' in measurement:
-                        return jsonify(error="no time provided")
-                    timestamp = datetime.fromtimestamp(time.mktime(time.strptime(measurement['time'],
-                                                                                 API_TIME_FORMAT)))
-                    for attr in aqxdb.ATTR_NAMES:
-                        if attr in measurement:
-                            try:
-                                aqxdb.add_measurement(cursor, system_id, attr, timestamp, measurement[attr])
-                            except:
-                                # only warn once per submission
-                                if not warned:
-                                    app.logger.warn('attempted to add data for existing time')
-                                    warned = True
-                conn.commit()
-                return jsonify(status="Ok")
-            except Exception, e:
-                app.logger.exception(e)
-                return jsonify(error="error in input document")
-        else:
-            return jsonify(error="attempt to access non-existing (or non-owned) system")
-    finally:
-        cursor.close()
-        conn.close()
-
-
-# This is an example for an API call that is authenticated with Google OAuth2
-# The call expects an authorization header with a Bearer token received
-# from Google OAuth2.
-@app.route('/api/v1/api_test', methods=['POST', 'GET'])
-@authorize
-def api_test(*args, **kwargs):
-    app.logger.debug('api_test(), user_id: %s', kwargs['google_id'])
-
-    # this is the authorization code
-    return jsonify(hallo='spencer')
-    
 
 if __name__ == '__main__':
     handler = logging.StreamHandler()

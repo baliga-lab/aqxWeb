f23f9fb1b388f6abbbf5a44445e9edfe9e7e1db2
Wei-ju Wu <weiju.wu@gmail.com>
2015-08-21 15:07:38 -0700

    add indexes and data

---
+++ aquaponics_data.sql
@@ -0,0 +1,13 @@
+insert into aquatic_organisms (name) values ('Tilapia');
+insert into aquatic_organisms (name) values ('Bluegill');
+insert into aquatic_organisms (name) values ('Shrimp');
+
+insert into crops (name) values ('Lettuce');
+insert into crops (name) values ('Strawberry');
+insert into crops (name) values ('Carrot');
+insert into crops (name) values ('Pea');
+
+insert into aqx_techniques (name) values ('Nutrient Film Technique (NFT)');
+insert into aqx_techniques (name) values ('Ebb and Flow (Media-based)');
+insert into aqx_techniques (name) values ('Floating Raft');
+insert into aqx_techniques (name) values ('Vertical Flow Through');

3c209ed89157b0bc88310bf68aa86cedd5c7257b
Wei-ju Wu <weiju.wu@gmail.com>
2015-09-02 14:34:49 -0700

    database changes, time seies extraction changes
     - added/updated catalog data
     - systems have a status field
     - time series data is now extracted and sorted correctly

--- aquaponics_data.sql
+++ aquaponics_data.sql
@@ -1,4 +1,8 @@
-insert into aquatic_organisms (name) values ('Tilapia');
+insert into aquatic_organisms (name) values ('Mozambique Tilapia');
+insert into aquatic_organisms (name) values ('Nile Tilapia');
+insert into aquatic_organisms (name) values ('Blue Tilapia');
+insert into aquatic_organisms (name) values ('Koi');
+insert into aquatic_organisms (name) values ('Goldfish');
 insert into aquatic_organisms (name) values ('Bluegill');
 insert into aquatic_organisms (name) values ('Shrimp');
 
@@ -12,3 +16,8 @@ insert into aqx_techniques (name) values ('Nutrient Film Technique (NFT)');
 insert into aqx_techniques (name) values ('Ebb and Flow (Media-based)');
 insert into aqx_techniques (name) values ('Floating Raft');
 insert into aqx_techniques (name) values ('Vertical Flow Through');
+
+insert into growbed_media (name) values ('Clay Pebbles');
+insert into growbed_media (name) values ('Coconut Coir');
+insert into growbed_media (name) values ('Seed Starter Plugs');
+
--- aqxdb.py
+++ aqxdb.py
@@ -75,7 +75,9 @@ def systems_and_latest_measurements(cursor, user_id):
 
 def get_measurement_series(cursor, sys_uid, attr):
     cursor.execute("select time, value from " + meas_table_name(sys_uid, attr) + " order by time desc limit 100")
-    return [[str(time), float(value)] for time, value in cursor.fetchall()]
+    result = [[str(time), float(value)] for time, value in cursor.fetchall()]
+    result = result[::-1]  # we need to reverse the list for displaying the right time order
+    return result
 
 
 def create_aquaponics_system(cursor, user_pk, name):

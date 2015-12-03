f4485adf2a3011a4ee731ad666f5eda70eb63d64
Wei-ju Wu <weiju.wu@gmail.com>
2015-12-03 12:43:05 -0800

    added support for time picker
    browsers that do not support input time are using ptTimeSelect instead

--- app.py
+++ app.py
@@ -431,9 +431,24 @@ def delete_system(system_uid=None):
 
 FALLBACK_TIME_FORMAT = '%Y-%m-%dT%H:%MZ'
 
+def make_time24(timestr):
+    if timestr.endswith('AM') or timestr.endswith('PM'):
+        comps = timestr.split()
+        ampm = comps[1]
+        timecomps = map(int, comps[0].split(':'))
+        hours = timecomps[0]
+        minutes = timecomps[1]
+        if hours == 12:
+            hours = 0 if ampm == 'AM' else hours
+        elif ampm == 'PM':
+            hours += 12
+        return "%02d:%02d" % (hours, minutes)
+    else:
+        return timestr
 
 def get_form_time(datestr, timestr):
     if timestr:
+        timestr = make_time24(timestr)
         s = "%sT%sZ" % (datestr, timestr)
     else:
         s = "%sT00:00:00Z" % datestr
